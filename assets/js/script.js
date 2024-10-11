bcfe0949c7f5f14cf5169842d3fe86d32f432cbc
Ioanna Arkoudi <jsim1v1v@students.codeinstitute.net>
2024-10-11 20:34:18 +0000

    Apply changes in script js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded" function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for let (i = 0; i < buttons.length; i++)
+})
+
 function runGame() {
 
 }
@@ -31,5 +40,5 @@ function displayMultiplyQuestion() {
 }
 
 function displayDivisionQuestion() {
-    
+
 }
